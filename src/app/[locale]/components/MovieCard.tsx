323be39a3755f49e79df870d1dd886bc3548baa2
Rubén Y <rubenmeju@outlook.es>
2024-07-01 22:36:50 +0200

    fix fill=true

--- src/app/[locale]/components/MovieCard.tsx
+++ src/app/[locale]/components/MovieCard.tsx
@@ -16,11 +16,11 @@ const MovieCard: React.FC<MovieCardProps> = async ({ movie }) => {
           <Image
             src={`${process.env.NEXT_PUBLIC_URL_IMAGES_TMDB}${movie.poster_path}`}
             alt={`Poster of ${movie.title}`}
-            layout="fill"
-            objectFit="cover"
+            fill={true}
             placeholder="blur"
             blurDataURL={`${process.env.NEXT_PUBLIC_URL_IMAGES_TMDB}${movie.poster_path}`}
             className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         ) : (
           <div className="absolute inset-0 bg-gray-200 flex items-center justify-center">
