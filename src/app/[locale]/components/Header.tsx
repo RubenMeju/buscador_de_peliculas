6ffe0fe206af8bdd6ab665d939e75113dcbebe63
Rubén Y <rubenmeju@outlook.es>
2024-07-02 09:36:46 +0200

    header fixed scroll bg-black

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,10 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
 import LocaleSwitcher from "./locale/LocaleSwitcher";
 import ThemeToggleButton from "./ThemeToggleButton";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="fixed top-0 left-0 z-20 w-full h-18">
-      <div className=" w-4/5 m-auto flex justify-between items-center lg:w-2/4">
+    <div
+      className={`fixed top-0 left-0 z-20 w-full h-18 transition-colors duration-300 ${
+        isScrolled ? "bg-black" : ""
+      }`}
+    >
+      <div className="w-4/5 m-auto flex justify-between items-center lg:w-2/4">
         <span className="dark:text-white">Movies APP</span>
         <div className="flex gap-2">
           <LocaleSwitcher />
